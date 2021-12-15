43eed96eb332d13cd682eb2ea9298dfcacec085d
Johnatas Rabelo <you@example.com>
2021-12-15 14:02:16 -0300

    add files

--- django_main/django.Dockerfile
+++ django_main/django.Dockerfile
@@ -1,26 +1,23 @@
 FROM python:3.8-slim
 
-ENV PYTHONUNBUFFERED 1 \
-    PIP_DEFAULT_TIMEOUT=100 \
-    PIP_DISABLE_PIP_VERSION_CHECK=1 \
-    PIP_NO_CACHE_DIR=1 \
+ENV PYTHONUNBUFFERED 1
 
 RUN groupadd user && useradd --create-home --home-dir /home/user -g user user
-WORKDIR /var/www/app
+WORKDIR /app
 
 # Install system dependencies
 RUN apt-get update && apt-get install gcc build-essential python3-psycopg2 libpq-dev -y && \
     python3 -m pip install --no-cache-dir pip-tools
 
 RUN pip install psycopg2-binary
-COPY ./requirements.txt /var/www/app
+COPY ./requirements.txt /app
 RUN pip install -r requirements.txt
 
 # Clean the house
 RUN apt-get purge libpq-dev -y && apt-get autoremove -y && \
     rm /var/lib/apt/lists/* rm -rf /var/cache/apt/*
 
-COPY . /var/www/app
+COPY . /app
 
 USER user
 
--- django_main/domain/repositories.py
+++ django_main/domain/repositories.py
@@ -1,49 +1,44 @@
 # from channels.db import database_sync_to_async
-
 from django_main.domain.models import Customer
 
 
 class CustomerRepo:
-    async def get_customer(**kwargs):
+
+    async def get_customer(self, **kwargs):
         customer = await __get_customer(**kwargs)
         return customer[0]
 
-
-    async def get_last_customer():
+    async def get_last_customer(self):
         customer = await __get_customer(is_last=True)
         return customer
 
-
-    async def create_customer(**kwargs):
+    async def create_customer(self, **kwargs):
         x = await __create_customer(**kwargs)
         customer = await __get_customer(is_last=True)
         return customer
 
-
-    async def update_customer(customer_id, **kwargs):
+    async def update_customer(self, customer_id: int, **kwargs):
         x = await __update_customer(customer_id, **kwargs)
         customer = await __get_customer(customer_id=customer_id)
         return customer
 
-
     @database_sync_to_async
-    def __get_customer(is_last=False, **kwargs):
+    def __get_customer(self, is_last=False, **kwargs):
         if is_last:
             return Customer.objects.last()
 
         return Customer.objects.filter(**kwargs).all()
 
-
     @database_sync_to_async
-    def __create_customer(**kwargs):
+    def __create_customer(self, **kwargs):
         Customer.objects.create(**kwargs)
 
 
     @database_sync_to_async
-    def __update_customer(customer_id, **kwargs):
+    def __update_customer(self, customer_id: int, **kwargs):
         Customer.objects.filter(id=customer_id).update(**kwargs)
 
 
     @database_sync_to_async
-    def delete_customer(customer_id):
+    def delete_customer(self, customer_id: int):
         Customer.objects.delete(id=customer_id)
